fba5b81cc2eb2adc4d2fe2e947a93c7c462d395f
KVN149 <idooll@bk.ru>
2024-09-22 22:34:15 +0400

    Add files via upload

---
+++ module_3_1_1.py
@@ -0,0 +1,28 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+    return
+
+
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    for lst in list_to_search:
+        if lst.lower() in string.lower():
+            return True
+        else:
+            return False
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
